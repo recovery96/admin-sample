796094e2b3630ffdbd541acc8c9f430a9abee91c
Taekyeom Lee <tkyeom96@gmail.com>
2024-02-19 22:21:23 +0900

    feat: Install rollup-plugin-visualizer for bundle analysis

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from 'path'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      filename: './dist/report.html',
+      open: true,
+      brotliSize: true,
+    }),
+  ],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
