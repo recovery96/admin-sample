d1c19227cecb0f7df055e4886cad6caa0e72184c
Taekyeom Lee <tkyeom96@gmail.com>
2024-02-19 22:27:21 +0900

    feat: Optimize bundle splitting for better caching

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from 'path'
 import { visualizer } from 'rollup-plugin-visualizer'
@@ -7,6 +7,7 @@ import { visualizer } from 'rollup-plugin-visualizer'
 export default defineConfig({
   plugins: [
     react(),
+    splitVendorChunkPlugin(),
     visualizer({
       filename: './dist/report.html',
       open: true,
@@ -19,5 +20,24 @@ export default defineConfig({
   server: {
     host: '127.0.0.1',
     port: 8080,
+  },
+  preview: {
+    host: '127.0.0.1',
+    port: 8080,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-libs': ['react', 'react-dom'],
+          'routing-libs': ['react-router-dom'],
+          'ui-libs': ['@mui/icons-material', '@mui/lab', '@mui/material'],
+          'state-management-libs': ['@tanstack/react-query', 'recoil'],
+          'form-libs': ['react-hook-form', '@hookform/resolvers', 'zod'],
+          'date-libs': ['dayjs'],
+          'utils-libs': ['axios'],
+        },
+      },
+    },
   },
 })
